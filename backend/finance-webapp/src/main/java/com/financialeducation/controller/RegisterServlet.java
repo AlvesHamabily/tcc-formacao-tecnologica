3edd7fdefeaaae105b66d5f80d0479e576241e3f
Murilo Enzo <murilo@whoami.com>
2025-03-29 00:34:19 -0400

    vincula tela de login e registro

--- backend/finance-webapp/src/main/java/com/financialeducation/controller/RegisterServlet.java
+++ backend/finance-webapp/src/main/java/com/financialeducation/controller/RegisterServlet.java
@@ -18,25 +18,32 @@ public class RegisterServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
+        String username = request.getParameter("nome");
         String password = request.getParameter("password");
+        String email = request.getParameter("email");
+
 
         try (Connection con = com.financialeducation.model.Conexao.getConnection()) {
             PreparedStatement sl = con.prepareStatement("SELECT * FROM users WHERE username=?");
             sl.setString(1, username);
             ResultSet rs = sl.executeQuery();
+
+            PreparedStatement sll = con.prepareStatement("SELECT * FROM users WHERE email=?");
+            sll.setString(1, email);
+            ResultSet rss = sll.executeQuery();
             
-            if (!rs.next()) {
-	            PreparedStatement ps = con.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)");
+            if (!rs.next() | !rss.next()) {
+	            PreparedStatement ps = con.prepareStatement("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
 	            ps.setString(1, username);
 	            ps.setString(2, BCrypt.hashpw(password, BCrypt.gensalt(12)));
+	            ps.setString(3, email);
 	            ps.executeUpdate();
 	            
-	            response.sendRedirect("index.jsp?register=success");
+	            response.sendRedirect("index.html?register=success");
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            response.sendRedirect("register.jsp?error=1");
+            response.sendRedirect("cadastro.html?error=1");
         }
     }
 }
